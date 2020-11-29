14a3ab24a83ed6b5861c1b7953ce5413504fb3f2
GUSTAVO TIETGEN <gustavo.tietgen01@fatec.sp.gov.br>
2020-11-29 18:42:17 -0300

    Arquitetura base do projeto.

---
+++ Application.WebAPI/Application.Domain/BaseDomain.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Domain
+{
+
+    /// <summary>
+    /// Entidade base para as classes do sistema
+    /// </summary>
+    public class BaseDomain
+    {
+        public int ID { get; set; }
+        public string Descricao { get; set; }
+    }
+}
